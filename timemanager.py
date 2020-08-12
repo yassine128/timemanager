4083b00f29ff3833a33b9d8ffeacdd549affe07a
yassine128 <yassineseddaoui@gmail.com>
2020-08-12 14:36:26 -0400

    Better code

--- timemanager.py
+++ timemanager.py
@@ -35,7 +35,8 @@ def task_taker():
                 print('=========================================================================')
                 print('TASK NUMBER '+str(i+1))
                 task = str(input('What is your task?: '))
-                timer = input('How much time will you take to finish after the last task? (e.g: 10 minutes, ,1 hour, 1 hour and 10 minutes): ')
+                timer = input('How much time will you take to finish after the last task? (e.g: 10 minutes, 1 hour, 1 hour and 10 minutes): ')
+                timersplit = timer.split()
                 print('=========================================================================')
                 taskname.append(task)
                 tasktime.append(timer)
@@ -43,8 +44,10 @@ def task_taker():
 
     except:
         print('Enter an integer.')
-
-try:
+v1=1
+minutepast = 0
+hourpast = 0
+if v1==1:
     m=0
     while True:
         if v2 == 0:
@@ -123,9 +126,7 @@ try:
                         m=m+1
 
                 if 'minutes' in timer and 'hour' not in timer:
-                    print('ok')
                     if 'hours' not in timer:
-                        print('o2k')
                         minuteindex = timer.index('minutes')
                         nminute = int(timer[minuteindex-1]) #10
                     
@@ -136,9 +137,18 @@ try:
 
         if v2 == 1:
             if ntask != m:
-                timer = tasktime[m]
-                timer = timer.lower()
-                timer = timer.split() #1 hour and 10 minutes
+                timer = tasktime[m] #1 hour and 10 minutes
+                timer = timer.lower() #1 hour and 10 minutes
+                timer = timer.split() #['1', 'hour', 'and', '10', 'minutes']
+                timerpast = tasktime[m-1] #1 minute
+                timerpast = timerpast.split()
+                for z in range(len(timerpast)):
+                    if timerpast[z] == 'minute' or timerpast[z] == 'minutes':
+                        minutepast = int(timerpast[z-1])
+                        minutepast = minutepast*60
+                    if timerpast[z] == 'hour' or timerpast[z] == 'hours':
+                        hourpast = int(timerpast[z-1])
+                        hourpast = (hourpast*60)*60
                 
                 if 'hour' in timer:
                     hourindex = timer.index('hour')
@@ -149,7 +159,7 @@ try:
 
                         sleephour = (nhour*60)*60
                         sleepminute = (nminute*60)
-                        time.sleep(sleephour+sleepminute)
+                        time.sleep(abs((sleephour+sleepminute)-(minutepast+hourpast)))
                         notification()
                         m=m+1
 
@@ -159,12 +169,12 @@ try:
 
                         sleephour = (nhour*60)*60
                         sleepminute = (nminute*60) 
-                        time.sleep(sleephour+sleepminute)
+                        time.sleep(abs((sleephour+sleepminute)-(minutepast+hourpast)))
                         notification()
                         m=m+1
                     else:
                         sleephour = (nhour*60)*60 
-                        time.sleep(sleephour)
+                        time.sleep(abs(sleephour-hourpast))
                         notification()
                         m=m+1
 
@@ -177,7 +187,7 @@ try:
 
                         sleephour = (nhour*60)*60
                         sleepminute = (nminute*60) 
-                        time.sleep(sleephour+sleepminute)
+                        time.sleep(abs((sleephour+sleepminute)-(minutepast+hourpast)))
                         notification()
                         m=m+1
 
@@ -187,12 +197,12 @@ try:
 
                         sleephour = (nhour*60)*60
                         sleepminute = (nminute*60) 
-                        time.sleep(sleephour+sleepminute)
+                        time.sleep(abs((sleephour+sleepminute)-(minutepast+hourpast)))
                         notification()
                         m=m+1
                     else:
                         sleephour = (nhour*60)*60 
-                        time.sleep(sleephour)
+                        time.sleep(abs(sleephour-hourpast))
                         notification()
                         m=m+1
 
@@ -202,21 +212,19 @@ try:
                         nminute = int(timer[minuteindex-1]) #10
 
                         sleepminute = (nminute*60)
-                        time.sleep(sleepminute)
+                        time.sleep(abs(sleepminute-minutepast))
                         notification()
                         m=m+1
 
                 if 'minutes' in timer and 'hour' not in timer:
                     if 'hours' not in timer:
-                        print('o2k')
                         minuteindex = timer.index('minutes')
                         nminute = int(timer[minuteindex-1]) #10
-                    
-                        sleepminute = (nminute*60)
-                        time.sleep(sleepminute)
+                        sleepminute = (nminute*60) 
+                        time.sleep(abs(sleepminute-minutepast))
                         notification() 
                         m=m+1
 
 
-except:
-    print('An error occured... please restart the progam.')
+#except:
+    #print('An error occured... please restart the progam.')
